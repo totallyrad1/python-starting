253d24b567a9f904011d8e90460cc16aaebcc714
Asaad Naji <asnaji@e2r5p15.1337.ma>
2024-01-28 10:13:55 +0100

    updated

--- whatis.py
+++ whatis.py
@@ -2,20 +2,20 @@ import sys
 x = 0
 n = sys.argv
 for i in n:
-	x += 1
-if(x == 1):
-	exit()
-if(x > 2):
-	print("AssertionError: more than one argument is provided")
-	exit()
+    x += 1
+if x == 1:
+    exit()
+if x > 2:
+    print("AssertionError: more than one argument is provided")
+    exit()
 try:
-	x = int(n[1])
+    x = int(n[1])
 except ValueError:
-	print("AssertionError: argument is not an integer")
-	exit()
+    print("AssertionError: argument is not an integer")
+    exit()
 if x < 0:
-	x = x * -1
+    x = x * -1
 if x % 2 == 0:
-	print("I'm Even")
+    print("I'm Even")
 else:
-	print("I'm Odd.")
+    print("I'm Odd.")
