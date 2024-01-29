1bff0ae26f5b1133b151ea03caa0d5ebb8edfee1
Asaad Naji <asnaji@e2r5p15.1337.ma>
2024-01-29 09:47:19 +0100

    hehe

--- building.py
+++ building.py
@@ -9,11 +9,11 @@ def treatstr(argument: any) -> int:
     charcount = 0
     lowcount = 0
     uppcount = 0
-    spcount = 0
+    spcount = 0	
     digcount = 0
     puncount = 0
     for s in argument:
-        if s in lowerchars or s in upperchars or s in digits or s in space or s in punc:
+        if s in lowerchars or upperchars or digits or space or punc:
             charcount += 1
         if s in lowerchars:
             lowcount += 1
@@ -35,26 +35,14 @@ def treatstr(argument: any) -> int:
 
 def main():
     args = sys.argv
-    i = 0
     x = 0
-    lowerchars = 'abcdefghijklmnopqrstuvwxyz'
-    upperchars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    digits = '0123456789'
-    punc = '.?!,:;-[]{}()`"\''
-    space = ' '
-    charcount = 0
-    lowcount = 0
-    uppcount = 0
-    spcount = 0
-    digcount = 0
-    puncount = 0
     for f in args:
         x += 1
     if x > 2:
         print("AssertionError: more than one arg provided")
         exit()
     if x == 2:
-        treatstr(args)
+        treatstr(args[1])
     else:
         treatstr(input("What is the text to count?\n"))
 	
